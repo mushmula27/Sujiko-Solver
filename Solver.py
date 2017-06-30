66a7d3b2546b53a5717c682407cc9c93e9a37462
Zhanet <ivanova.zhanet@gmail.com>
2017-06-30 16:56:33 +0100

    Created class for Blocks

---
+++ Solver.py
@@ -0,0 +1,15 @@
+class Block(object):
+    def __init__(self, center, numbers):
+        self.center = center
+        self.top_left = numbers[0]
+        self.top_right = numbers[1]
+        self.bottom_left = numbers[2]
+        self.bottom_right = numbers[3]
+        self.sum = 0
+
+    def add_numbers(self):
+        self.sum = self.top_right + self.top_left + self.bottom_right + self.bottom_left
+
+    def compare_numbers(self):
+        self.add_numbers()
+        return self.center == self.sum
